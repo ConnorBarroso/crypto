48f2975fdec7a015663662f78e5f1493747cf931
ConnorBarroso <connor.barroso@gmail.com>
2022-02-02 17:15:52 -0500

    made folders with structure and added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+
+import { CoinList, CoinPage, Portfolio } from 'pages';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <Router>
+        <Switch>
+          <Route path='/' element={CoinList}/>
+          <Route path='/portfolio' element={Portfolio}/>
+          <Route path='/coin/:coinId' element={CoinPage}/>
+        </Switch>
+      </Router>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class PortfolioPage extends React.Component{
+    render(){
+        return(
+            <div>This is the Portfolio page</div>
+        )
+    }
+}
+
+export default PortfolioPage
